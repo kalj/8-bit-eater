c6c4b2bb740e18ebc55f367e66285408a31d1166
Karl Ljungkvist <k.ljungkvist@gmail.com>
2020-02-26 01:38:18 +0100

    add option to set up 7-segment display for signed decimal

--- 7-segment-hex-decode.py
+++ 7-segment-hex-decode.py
@@ -51,21 +51,82 @@ hexsegm = [(1 << 0) | (1 << 1) | (1 << 2) | (0 << 3) | (1 << 4) | (1 << 5) | (1 
            (1 << 0) | (0 << 1) | (0 << 2) | (0 << 3) | (0 << 4) | (1 << 5) | (1 << 6) | (1 << 7)  # F
            ]
 
+minus_segm = (0 << 0) | (0 << 1) | (0 << 2) | (0 << 3) | (0 << 4) | (0 << 5) | (0 << 6) | (1 << 7) # -
+
+
 
 import sys
 
 data = [None]*2048
 
 for i in range(256):
-    data[i] = hexsegm[i%16]
-    data[256+i] = hexsegm[(i//16)%16]
-    data[512+i] = hexsegm_off
-    data[768+i] = hexsegm_off
+    # hexadecimal
+    hexa = [hexsegm[i%16],
+            hexsegm[(i//16)%16],
+            hexsegm_off,
+            hexsegm_off]
 
-    data[1024+i] = hexsegm[i%10]
-    data[1280+i] = hexsegm[(i//10)%10] if (i>9) else hexsegm_off
-    data[1536+i] = hexsegm[(i//100)%10] if (i>99) else hexsegm_off
-    data[1792+i] = hexsegm_off
+    # signed decimal
+    if i > 127:
+        add_minus_sign = True
+        signed_abs = abs(i-256)
+    else:
+        add_minus_sign = False
+        signed_abs = i
+
+    signed_dec = [hexsegm[signed_abs%10],
+                  hexsegm_off,
+                  hexsegm_off,
+                  hexsegm_off]
+
+    if signed_abs>9:
+        signed_dec[1] = hexsegm[(signed_abs//10)%10]
+    elif add_minus_sign:
+        signed_dec[1] = minus_segm
+        add_minus_sign = False
+
+    if signed_abs>99:
+        signed_dec[2] = hexsegm[(signed_abs//100)%10]
+    elif add_minus_sign:
+        signed_dec[2] = minus_segm
+        add_minus_sign = False
+
+    if add_minus_sign:
+        signed_dec[3] = minus_segm
+        add_minus_sign = False
+
+    # unsigned decimal
+    unsigned_dec = [hexsegm[i%10],
+                    hexsegm_off,
+                    hexsegm_off,
+                    hexsegm_off]
+    if i>9:
+        unsigned_dec[1] = hexsegm[(i//10)%10]
+    if (i>99):
+        unsigned_dec[2] = hexsegm[(i//100)%10]
+
+
+    # write stuff
+
+    # data[i]     = hexa[0]
+    # data[256+i] = hexa[1]
+    # data[512+i] = hexa[2]
+    # data[768+i] = hexa[3]
+
+    data[i]     = signed_dec[0]
+    data[256+i] = signed_dec[1]
+    data[512+i] = signed_dec[2]
+    data[768+i] = signed_dec[3]
+
+    data[1024+i] = unsigned_dec[0]
+    data[1280+i] = unsigned_dec[1]
+    data[1536+i] = unsigned_dec[2]
+    data[1792+i] = unsigned_dec[3]
+
+
+sys.stdout.buffer.write(bytes(data))
+
+# Stuff for debug printing as ascii art
 
 def lines_from_7segm_digit(d0):
     lines = [ " "+ ("_____" if ((d0>>5) & 1) else "     "),
@@ -94,6 +155,3 @@ def print_7segm_number(d0,d1,d2):
 
 #     offset = 1024 if decimal else 0
 #     print_7segm_number(data[offset+i],data[offset+256+i],data[offset+512+i])
-
-
-sys.stdout.buffer.write(bytes(data))
